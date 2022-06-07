39f466a3a993e3c5fd5326303790d44f005e0253
MignolaRodrigo <mignolarodrigo@gmail.com>
2022-06-07 15:00:22 -0300

    Update view register
    Se agrego un campo en la pantalla de regisro

--- Trivia.TDP/Vistas/Register.Designer.cs
+++ Trivia.TDP/Vistas/Register.Designer.cs
@@ -46,6 +46,8 @@ namespace Trivia.TDP.Vistas
             this.label6 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -242,6 +244,25 @@ namespace Trivia.TDP.Vistas
             this.pictureBox2.TabIndex = 47;
             this.pictureBox2.TabStop = false;
             // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(232, 184);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(99, 20);
+            this.textBox7.TabIndex = 49;
+            this.textBox7.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(80, 184);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 17);
+            this.label8.TabIndex = 48;
+            this.label8.Text = "Fecha de Nacimiento : ";
+            this.label8.UseWaitCursor = true;
+            // 
             // Register
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -295,5 +316,7 @@ namespace Trivia.TDP.Vistas
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
     }
 }
