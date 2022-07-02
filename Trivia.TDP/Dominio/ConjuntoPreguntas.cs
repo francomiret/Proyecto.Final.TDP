f6ab1cc00bf5d3d76fbec636b66583518a2a05e3
franco <francomiret11@gmail.com>
2022-07-02 00:35:17 -0300

    feat

---
+++ Trivia.TDP/Dominio/ConjuntoPreguntas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dominio
+{
+    public class ConjuntoPreguntas
+    {
+        public String Id { get; set; }
+        public String Nombre { get; set; }
+        public float TiempoEsperadoRespuesta { get; set; }
+        public Dificultad Dificultad { get; set; }
+        public Categoria Categoria { get; set; }
+
+
+        public ConjuntoPreguntas(string pNombre, float pTiempoEsperadoRespuesta, Dificultad pDificultad, Categoria pCategoria)
+        {
+            this.Nombre = pNombre;
+            this.Dificultad = pDificultad;
+            this.Categoria = pCategoria;
+            this.TiempoEsperadoRespuesta = pTiempoEsperadoRespuesta;
+        }
+
+        public ConjuntoPreguntas(string pNombre, Dificultad pDificultad, Categoria pCategoria)
+        {
+            this.Nombre = pNombre;
+            this.Dificultad = pDificultad;
+            this.Categoria = pCategoria;
+            this.TiempoEsperadoRespuesta = 20;
+        }
+
+        public ConjuntoPreguntas() { }
+    }
+}
