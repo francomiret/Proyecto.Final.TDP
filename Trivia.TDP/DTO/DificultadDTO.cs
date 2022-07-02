bc89df6dfcbec9c72c93330df895141cfea24a99
Felipe <cestaufelipe08@gmail.com>
2022-07-02 01:28:24 -0300

    Add DTOs

---
+++ Trivia.TDP/DTO/DificultadDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia.TDP.DTO
+{
+    class DificultadDTO
+    {
+        public int DificultadId { get; set; }
+        public string descripcion { get; set; }
+        public double peso { get; set; }
+    }
+}
---
+++ Trivia.TDP/DTO/RespuestaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia.TDP.DTO
+{
+    class RespuestaDTO
+    {
+        public int RespuestaId { get; set; }
+
+        public string descripcion { get; set; }
+
+        public bool correcta { get; set; }
+    }
+
+}
