f9740f320ed7ea918e91c787fa9b20cab0804b24
Felipe <cestaufelipe08@gmail.com>
2022-07-03 01:12:44 -0300

    finished opentDB connection

---
+++ Trivia.TDP/Controladores/Errores/DataNotFound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Trivia.TDP.Controladores
+{
+    [Serializable]
+    internal class DataNotFound : Exception
+    {
+        public DataNotFound()
+        {
+        }
+
+        public DataNotFound(string message) : base(message)
+        {
+        }
+
+        public DataNotFound(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DataNotFound(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
